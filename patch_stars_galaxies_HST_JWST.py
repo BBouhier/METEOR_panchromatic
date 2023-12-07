8f2cb343cea7fa2a6b7c3af487792bb51132b07f
Brieuc Bouhier <121092948+Peticitron@users.noreply.github.com>
2023-12-07 15:43:25 +0100

    Update patch_stars_galaxies_HST_JWST.py

--- patch_stars_galaxies_HST_JWST.py
+++ patch_stars_galaxies_HST_JWST.py
@@ -15,6 +15,8 @@ import astropy.units as u
 from astropy.coordinates import SkyCoord
 from astroquery.gaia import Gaia
 from astropy.table import Table
+from pathlib import Path
+
 Gaia.MAIN_GAIA_TABLE = "gaiadr3.gaia_source"
 
 import numpy as np
@@ -23,8 +25,6 @@ from astropy.io import fits
 from astropy.wcs import WCS
 import os 
 import re
-
-plt.close('all')
 
 def gaia(filename):
     hst = re.search(r'sci', filename)
@@ -46,21 +46,17 @@ def gaia(filename):
     return RA_stars, DEC_stars, parallax_stars, pmra_stars, pmdec_stars
 
 def get_files_in_folder(folder_path):
-    folder_path = os.path.abspath(folder_path)
-
-    if os.path.exists(folder_path) and os.path.isdir(folder_path):
-        files_in_folder = [os.path.join(folder_path, file) for file in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, file))]
-        return files_in_folder
-    else:
-        print(f"Error: folder '{folder_path}' does not exist.")
-        return None
+    p = Path(folder_path)
+    HST = p.glob('*sci.fits')
+    JWST = p.glob('*anchored.fits')
+    TXT = p.glob('*.txt')
+    return list(HST), list(JWST), list(TXT)
     
-
 def create_folder(folder):
     first_path = folder[0]
     galaxy = os.path.dirname(first_path)
     parent_directory_path = os.path.dirname(os.path.dirname(first_path))
-    new_folder_name = os.path.basename(galaxy) + '_patch_correctionned'
+    new_folder_name = os.path.basename(galaxy) + '_patch_corrected'
     new_folder_path = os.path.join(parent_directory_path, new_folder_name)
 
     if os.path.exists(new_folder_path):
@@ -91,209 +87,177 @@ def patch_stars(folder, plot_result = True):
     None.
 
     """
-    folder = get_files_in_folder(folder)
-    
-    created_folder, new_folder_path = create_folder(folder)
-    
-    txt_galaxies = next((file for file in folder if "txt_galaxies" in file), None)
-    
-    if not txt_galaxies:
-        print("Error: missing .txt file to patch galaxies")
-        
-    data = Table.read(txt_galaxies, format='ascii')
-    
-    query = gaia(folder[0])
-    
+    HST, JWST, TXT = get_files_in_folder(folder)
+    created_folder, new_folder_path = create_folder(HST)
+    table = Table.read(TXT[0],format='ascii')
+    for_query = str(HST[0])
+    query = gaia(for_query)
     ra_stars, dec_stars, parallax_stars, pmra_stars, pmdec_stars = query[0], query[1], query[2], query[3], query[4]
 
     ra_new = []
     dec_new = []
     threshold = 4.8e-8
-    r = 10 #radius of the patch for stars
+    r = 10 
     for l in range (len(parallax_stars)):
         if parallax_stars[l]>threshold:
             ra_new.append(ra_stars[l])
             dec_new.append(dec_stars[l])
-           
-    for filename in folder:
-        jwst = re.search(r'anchored', filename)
-        hst = re.search(r'sci', filename)
-        text = re.search(r'.txt', filename)
+
+    for filename in HST:
+        filename = str(filename)
         output_filename = os.path.join(new_folder_path, os.path.basename(filename).replace('.fits', '_star_galaxy_corrected.fits'))
+        hdu = fits.open(filename)[0]
+        image_open = hdu.data
+        hdul = hdu.header
+        image_open = hdul['PHOTFNU'] * image_open * 1e3
+        wavelength = extract_lambda(filename)
+        lambdaa = wavelength[0]
         
-        if hst:
-            hdu = fits.open(filename)[0]
-            image_open = hdu.data
-            hdul = hdu.header
-            wavelength = extract_lambda(filename)
-            lambdaa = wavelength[0]
-            
-            a_ell_arsec = []
-            b_ell_arsec = []
-            
-            for a_ell in data['a']:
-                convert_a = a_ell/(hdul['CD2_2']*3600)
-                a_ell_arsec.append(convert_a)
-                
-            for b_ell in data['b']:
-                convert_b = b_ell/(hdul['CD2_2']*3600)
-                b_ell_arsec.append(convert_b)
-            
-            ra_g, dec_g, a_g, b_g, theta_g = data['ra'], data['dec'], a_ell_arsec, b_ell_arsec, data['theta']
-            theta_rad = np.radians(theta_g)
-            wcs = WCS(hdu.header)
-            
-            for l in range (len(ra_new)): 
-                x_source, y_source = wcs.wcs_world2pix(ra_new[l], dec_new[l], 1)
-                x_source= int(x_source)
-                y_source = int(y_source)
-                length_rec_half = int(3*r)
-                width_rec_half = int(3*r)
-                noise_array = []
-            
-                for i in range (x_source-length_rec_half,x_source+length_rec_half):
-                    for j in range (y_source-width_rec_half,y_source+width_rec_half):
-                        if (i-x_source)**2/r**2 + (j-y_source)**2/r**2 >1:
-                            noise_array.append(image_open[i,j])
-
-                mean = np.mean(noise_array)
-                std = np.std(noise_array)
-                
-                for i in range (x_source-length_rec_half,x_source+length_rec_half):
-                    for j in range (y_source-width_rec_half,y_source+width_rec_half):    
-                        if (i-x_source)**2/r**2 + (j-y_source)**2/r**2 <1:
-                            image_open[i,j]= np.random.normal(mean,std)
-
-            for l in range (len(ra_g)): 
-                y_source, x_source = wcs.wcs_world2pix(ra_g[l], dec_g[l], 1)
-                x_source = int(x_source)
-                y_source = int(y_source)
-                length_rec_half = int(3*a_g[l])
-                width_rec_half = int(3*b_g[l])
-                noise_array = []
-            
-                for i in range (x_source-length_rec_half,x_source+length_rec_half):
-                    for j in range (y_source-width_rec_half,y_source+width_rec_half):
-                        if ((np.cos(theta_rad[l])*(x_source-i)+np.sin(theta_rad[l])*(y_source-j))**2/a_g[l]**2 
-                            + (np.sin(theta_rad[l])*(x_source-i)-np.cos(theta_rad[l])*(y_source-j))**2/b_g[l]**2) >1:
-                            noise_array.append(image_open[i,j])
-
-                mean = np.mean(noise_array)
-                std = np.std(noise_array)
-                
-                for i in range (x_source-length_rec_half,x_source+length_rec_half):
-                    for j in range (y_source-width_rec_half,y_source+width_rec_half):
-                        if ((np.cos(theta_rad[l])*(x_source-i)+np.sin(theta_rad[l])*(y_source-j))**2/a_g[l]**2 
-                            + (np.sin(theta_rad[l])*(x_source-i)-np.cos(theta_rad[l])*(y_source-j))**2/b_g[l]**2) <1:
-                            image_open[i,j]= np.random.normal(mean,std)
+        a_ell_arsec = []
+        b_ell_arsec = []
         
-            fits.writeto(output_filename, image_open, header=fits.getheader(filename), overwrite=True)
-            print('New file created:', output_filename)
+        for a_ell in table['a']:
+            convert_a = a_ell/(hdul['CD2_2']*3600)
+            a_ell_arsec.append(convert_a)
             
-        elif jwst:
-            hdu = fits.open(filename)[1]
-            image_open = hdu.data
-            hdul = hdu.header
-            wavelength = extract_lambda(filename)
-            lambdaa = wavelength[0]
-            
-            a_ell_arsec = []
-            b_ell_arsec = []
-            
-            for a_ell in data['a']:
-                convert_a = a_ell/(hdul['CDELT1']*3600)
-                a_ell_arsec.append(convert_a)
-                
-            for b_ell in data['b']:
-                convert_b = b_ell/(hdul['CDELT1']*3600)
-                b_ell_arsec.append(convert_b)
-            
-            ra_g, dec_g, a_g, b_g, theta_g = data['ra'], data['dec'], a_ell_arsec, b_ell_arsec, data['theta']
-            theta_rad = np.radians(theta_g)
-            wcs = WCS(hdu.header)
-
-            for l in range(len(ra_g)):
-                y_source, x_source = wcs.wcs_world2pix(ra_g[l], dec_g[l], 1)
-                x_source = int(x_source)
-                y_source = int(y_source)
-                length_rec_half = int(3 * a_g[l])
-                width_rec_half = int(3 * b_g[l])
-                noise_array = []
-            
-                pixel_value = image_open[y_source, x_source]
-            
-                if not np.isnan(pixel_value):
-                    for i in range(x_source - length_rec_half, x_source + length_rec_half):
-                        for j in range(y_source - width_rec_half, y_source + width_rec_half):
-                            if ((np.cos(theta_rad[l]) * (x_source - i) + np.sin(theta_rad[l]) * (y_source - j))**2 / a_g[l]**2 +
-                                    (np.sin(theta_rad[l]) * (x_source - i) - np.cos(theta_rad[l]) * (y_source - j))**2 / b_g[l]**2) > 1:
-                                pixel = image_open[i, j]
-                                if not np.isnan(pixel):
-                                    noise_array.append(pixel)
-            
-                    mean = np.mean(noise_array)
-                    std = np.std(noise_array)
-            
-                    for i in range(x_source - length_rec_half, x_source + length_rec_half):
-                        for j in range(y_source - width_rec_half, y_source + width_rec_half):
-                            if ((np.cos(theta_rad[l]) * (x_source - i) + np.sin(theta_rad[l]) * (y_source - j))**2 / a_g[l]**2 +
-                                    (np.sin(theta_rad[l]) * (x_source - i) - np.cos(theta_rad[l]) * (y_source - j))**2 / b_g[l]**2) < 1 and not np.isnan(image_open[i, j]):
-                                image_open[i, j] = np.random.normal(mean, std)
-            
-            fits.writeto(output_filename, image_open, header=fits.getheader(filename), overwrite=True)
-            print('New file created:', output_filename)
-            
-        elif text:
-            pass
+        for b_ell in table['b']:
+            convert_b = b_ell/(hdul['CD2_2']*3600)
+            b_ell_arsec.append(convert_b)
         
-        else:
-            print(f"Error: Unknown instrument type for file {filename}. It should be either HST or JWST.")
-                
+        ra_g, dec_g, a_g, b_g, theta_g = table['ra'], table['dec'], a_ell_arsec, b_ell_arsec, table['theta']
+        theta_rad = np.radians(theta_g)
+        wcs = WCS(hdu.header)
+        
+        for l in range (len(ra_new)): 
+            x_source, y_source = wcs.wcs_world2pix(ra_new[l], dec_new[l], 1)
+            x_source= int(x_source)
+            y_source = int(y_source)
+            length_rec_half = int(3*r)
+            width_rec_half = int(3*r)
+            noise_array = []
+        
+            for i in range (x_source-length_rec_half,x_source+length_rec_half):
+                for j in range (y_source-width_rec_half,y_source+width_rec_half):
+                    if (i-x_source)**2/r**2 + (j-y_source)**2/r**2 >1:
+                        noise_array.append(image_open[i,j])
+
+            mean = np.mean(noise_array)
+            std = np.std(noise_array)
+            
+            for i in range (x_source-length_rec_half,x_source+length_rec_half):
+                for j in range (y_source-width_rec_half,y_source+width_rec_half):    
+                    if (i-x_source)**2/r**2 + (j-y_source)**2/r**2 <1:
+                        image_open[i,j]= np.random.normal(mean,std)
+
+        for l in range (len(ra_g)): 
+            y_source, x_source = wcs.wcs_world2pix(ra_g[l], dec_g[l], 1)
+            x_source = int(x_source)
+            y_source = int(y_source)
+            length_rec_half = int(3*a_g[l])
+            width_rec_half = int(3*b_g[l])
+            noise_array = []
+        
+            for i in range (x_source-length_rec_half,x_source+length_rec_half):
+                for j in range (y_source-width_rec_half,y_source+width_rec_half):
+                    if ((np.cos(theta_rad[l])*(x_source-i)+np.sin(theta_rad[l])*(y_source-j))**2/a_g[l]**2 
+                        + (np.sin(theta_rad[l])*(x_source-i)-np.cos(theta_rad[l])*(y_source-j))**2/b_g[l]**2) >1:
+                        noise_array.append(image_open[i,j])
+
+            mean = np.mean(noise_array)
+            std = np.std(noise_array)
+            
+            for i in range (x_source-length_rec_half,x_source+length_rec_half):
+                for j in range (y_source-width_rec_half,y_source+width_rec_half):
+                    if ((np.cos(theta_rad[l])*(x_source-i)+np.sin(theta_rad[l])*(y_source-j))**2/a_g[l]**2 
+                        + (np.sin(theta_rad[l])*(x_source-i)-np.cos(theta_rad[l])*(y_source-j))**2/b_g[l]**2) <1:
+                        image_open[i,j]= np.random.normal(mean,std)
+    
+        fits.writeto(output_filename, image_open, header=fits.getheader(filename), overwrite=True)
+        
         if plot_result:
-            if jwst:
-                fig, ax = plt.subplots(subplot_kw={'projection': wcs}, figsize=(8, 8))
-                ax.imshow(image_open)
-            
-                ra_new = []
-                dec_new = []
-                for l in range(len(parallax_stars)):
-                    if parallax_stars[l] > threshold:
-                        ra_new.append(ra_stars[l])
-                        dec_new.append(dec_stars[l])
-            
-                x, y = wcs.wcs_world2pix(ra_g, dec_g, 1)
-                ax.scatter(x, y, marker='D', color='Green', label='Galaxies')
-                ax.legend()
-                plt.title(f'JWST at {lambdaa} nm')
-                plot_filename = os.path.join(new_folder_path, f'JWST_{lambdaa}_plot.png')
-                plt.savefig(plot_filename)  
-                plt.close('all')
-            
-            elif hst:
-                vmin = np.percentile(image_open, 5)
-                vmax = np.percentile(image_open, 99.95)
+            vmin = np.percentile(image_open, 5)
+            vmax = np.percentile(image_open, 99.95)
+            fig, ax = plt.subplots(subplot_kw={'projection': wcs}, figsize=(8, 8))
+            ax.imshow(image_open, vmin=vmin, vmax=vmax)
+            x, y = wcs.wcs_world2pix(ra_new, dec_new, 1)
+            ax.scatter(y, x, marker='+', color='red', label='Stars')
+            x, y = wcs.wcs_world2pix(ra_g, dec_g, 1)
+            ax.scatter(x, y, marker='D', color='Green', label='Galaxies')
+            ax.legend()
+            plt.title(f'HST at {lambdaa} nm')   
+            plot_filename = os.path.join(new_folder_path, f'HST_{lambdaa}_plot.png')
+            plt.savefig(plot_filename) 
+            plt.close('all')
                 
-                fig, ax = plt.subplots(subplot_kw={'projection': wcs}, figsize=(8, 8))
-                ax.imshow(image_open, vmin=vmin, vmax=vmax)
+        print('New file created:', output_filename)
             
-                x, y = wcs.wcs_world2pix(ra_new, dec_new, 1)
-                ax.scatter(y, x, marker='+', color='red', label='Stars')
-                x, y = wcs.wcs_world2pix(ra_g, dec_g, 1)
-                ax.scatter(x, y, marker='D', color='Green', label='Galaxies')
-                ax.legend()
-                plt.title(f'HST at {lambdaa} nm')   
-                plot_filename = os.path.join(new_folder_path, f'HST_{lambdaa}_plot.png')
-                plt.savefig(plot_filename) 
-                plt.close('all')
-                
+    for filename in JWST:
+        filename = str(filename)
+        output_filename = os.path.join(new_folder_path, os.path.basename(filename).replace('.fits', '_star_galaxy_corrected.fits'))
+        hdu = fits.open(filename)[1]
+        image_open = hdu.data
+        header = hdu.header
+        convert = header['PIXAR_SR']
+        image_open = image_open * 1e9 * convert #pour passer de Mjy/ste en mjy
+        wavelength = extract_lambda(filename)
+        lambdaa = wavelength[0]
+
+        fits.writeto(output_filename, image_open, header = header, overwrite=True)
+        print('New file created:', output_filename)
+        
+        if plot_result:
+            fig, ax = plt.subplots(subplot_kw={'projection': wcs}, figsize=(8, 8))
+            ax.imshow(image_open)
+            plt.title(f'JWST at {lambdaa} nm')
+            plot_filename = os.path.join(new_folder_path, f'JWST_{lambdaa}_plot.png')
+            plt.savefig(plot_filename)  
+            plt.close('all')        
+    
     print("###############################################################################")
     print('All the corrected fits files and plots have been save in the following folder: ')
     print(new_folder_path)
     print("###############################################################################")
+    return new_folder_path
 
+def patch_nan(filename, x, y, r):
+    hdu = fits.open(filename)[0]
+    image_open = hdu.data
+    x_source, y_source = int(y), int(x)
+    length_rec_half = int(3 * r)
+    width_rec_half = int(3 * r)
+    noise_array = []
 
+    for i in range(x_source - length_rec_half, x_source + length_rec_half):
+        for j in range(y_source - width_rec_half, y_source + width_rec_half):
+            if (i - x_source)**2 / r**2 + (j - y_source)**2 / r**2 > 1:
+                noise_array.append(image_open[i, j])
+
+    mean = np.mean(noise_array)
+    std = np.std(noise_array)
+
+    for i in range(x_source - length_rec_half, x_source + length_rec_half):
+        for j in range(y_source - width_rec_half, y_source + width_rec_half):
+            if (i - x_source)**2 / r**2 + (j - y_source)**2 / r**2 < 1:
+                if np.isnan(image_open[i, j]):
+                    image_open[i, j] = np.random.normal(mean, std)
+
+    fits.writeto(filename, image_open, header=fits.getheader(filename), overwrite=True)
+
+    print("###############################################################################")
+    print('The corrected FITS file has been saved !')
+    print("###############################################################################")
+    
 if __name__ == "__main__":
     print('Hello there, here is a script to correct JWST and HST fits files')
     print("Please enter the path to the directory with all the files for one galaxy")
     folder = input("Path --> ")
-    patch_stars(folder)
+    filename_to_correct = patch_stars(folder) +'/ngc1087_F200W_anchored_star_galaxy_corrected.fits'
+    print(filename_to_correct)
+    x = 2827
+    y = 2994
+    r = 6
+    patch_nan(filename_to_correct, x, y, r)
+    x = 2832
+    y = 2735
+    r = 3
+    patch_nan(filename_to_correct, x, y, r)
